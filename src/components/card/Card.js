41e5b105c5eb8e129d2b9dd4cd56d23baca3cb05
Vladimir Gatara <vladimir.gatara@carbiotix.com>
2023-11-07 12:21:46 +0100

    abwel

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 import './card.css';
 import StarSvg from '../../images/icon-star.svg';
 
-const Card = () => {
+const Card = (props) => {
     return (
         <div className='container' role="main">
             <div className='card__container'>
@@ -11,25 +12,31 @@ const Card = () => {
                 <p>Please let us know how we did with your support request. All feedback is appreciated
                     to help us improve our offering!</p>
                 <div className='card__container_ratings'>
-                    <div className='card__container_ratings_number'>
+                    <div className='card__container_ratings_number'
+                    onClick={() => props.setRating(1)}>
                         <p>1</p>
                     </div>
-                    <div className='card__container_ratings_number '>
+                    <div className='card__container_ratings_number '
+                    onClick={() => props.setRating(2)}>
                         <p>2</p>
                     </div>
-                    <div className='card__container_ratings_number '>
+                    <div className='card__container_ratings_number '
+                    onClick={() => props.setRating(3)}>
                         <p>3</p>
                     </div>
-                    <div className='card__container_ratings_number '>
+                    <div className='card__container_ratings_number '
+                    onClick={() => props.setRating(4)}
+                    >
                         <p>4</p>
                     </div>
-                    <div className='card__container_ratings_number '>
+                    <div className='card__container_ratings_number '
+                    onClick={() => props.setRating(5)}>
                         <p>5</p>
                     </div>
                 </div>
-                <a href="../thankyou">
+                <Link to={props.rating === null ? "/" : "/ThankYou"}>
                 <button>
-                 Submit</button></a>
+                 Submit</button></Link>
             </div>
         </div>
     )
